a82284962e86f8083e096271c880f7963329353c
pimenvibritania <blackbrit13@gmail.com>
2017-12-12 12:14:48 +0700

    update index

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   delete "/member/logout" , to: 'users#logout'
 
   get "/member/index" , to: 'member#index'
+  get "/product/barang", to: 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
